2a52c8817c0bc5e4ac0868cc400e42dce65049b1
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-25 10:48:22 -0500

    changed error message in signin

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,6 +13,18 @@ export const signup = (formProps, callback) => async dispatch => {
   }
 };
 
+export const signin = (formProps, callback) => async dispatch => {
+  try {
+    const response = await axios.post('http://localhost:3090/signin', formProps);
+    localStorage.setItem('token', response.data.token);
+    dispatch({ type: AUTH_USER, payload: response.data.token });
+    callback();
+  } catch(err) {
+    callback(err.response);
+  }
+};
+
+
 export const signout = () => {
   localStorage.removeItem('token');
   return { type: AUTH_USER, payload: '' };
--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+import * as actions from '../../actions';
 
 class Signin extends Component {
-  render(){
+  state = { error : ''};
+  onSubmit = (formProps) => {
+    console.log('formProps:', formProps);
+    this.setState({error: ''})
+    this.props.signin(formProps, this.handleResponse);
+  }
+
+  handleResponse = err => {
+    if(err) {
+      return this.setState({error: 'Invalid login credentials.'})
+    }
+    this.props.history.push("/feature");
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <div>Sign in!</div>
-    )
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <fieldset>
+          <label>Email</label>
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="off"
+          />
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+            <Field
+              name="password"
+              type="password"
+              component="input"
+              autoComplete="off"
+            />
+        </fieldset>
+        <div>{this.state.error}</div>
+        <button>Sign in!</button>
+      </form>
+    );
   }
 }
-
-export default Signin;
+export default compose(
+  reduxForm({ form: 'signin'}),
+  connect(null, actions),
+)(Signin);
